21f7b382cee522875430ac18d38421f526a50517
RHormiere <romain.hormieregmail.com>
2014-10-07 15:35:49 +0200

    aaa

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var express = require('express')
-var app = express();
+var http = require('http');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('<h1>My "Hello" to the World!</h1>')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Salut tout le monde !');
+});
+server.listen(8080);
