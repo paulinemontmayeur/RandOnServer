69e8b440bc0a5d004e4a1e6561284f738507c0c6
Thiebaud Thomas <thiebaud.tom@gmail.com>
2014-10-09 08:41:22 +0200

    Change github repo

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var express = require('express')
+var app = express();
 
-app.use(express.static(__dirname + "/"))
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-var server = http.createServer(app)
-server.listen(port)
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
-console.log("http server listening on %d", port)
-
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
-
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
-  console.log("websocket connection open")
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
 })
